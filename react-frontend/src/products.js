451edb747cc08b3105bcf70c57b914e86d1a8018
Jeff Peng <jeffpengyj@hotmail.com>
2021-06-03 21:31:07 -0400

    Home Screen mock up on react

---
+++ react-frontend/src/products.js
@@ -0,0 +1,69 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Bento Cake 1',
+        image: '/images/cake1.jpeg',
+        description:
+            'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est',
+        color: 'Red',
+        category: 'BentoCake',
+        price: 60,
+        countInStock: 3,
+        rating: 4.5,
+        numReviews: 4,
+    },
+    {
+        _id: '2',
+        name: 'Bento Cake 2',
+        image: '/images/cake2.jpeg',
+        description:
+            'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est',
+        color: 'Orange',
+        category: 'BentoCake',
+        price: 100,
+        countInStock: 3,
+        rating: 4.5,
+        numReviews: 4,
+    },
+    {
+        _id: '3',
+        name: 'Bento Cake 3',
+        image: '/images/cake3.jpeg',
+        description:
+            'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est',
+        color: 'Yellow',
+        category: 'BentoCake',
+        price: 55,
+        countInStock: 5,
+        rating: 4.0,
+        numReviews: 2,
+    },
+    {
+        _id: '4',
+        name: 'Bento Cake 4',
+        image: '/images/cake4.jpeg',
+        description:
+            'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est',
+        color: 'Pink',
+        category: 'BentoCake',
+        price: 60,
+        countInStock: 3,
+        rating: 4.5,
+        numReviews: 4,
+    },
+    {
+        _id: '5',
+        name: 'Bento Cake 5',
+        image: '/images/cake5.jpeg',
+        description:
+            'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est',
+        color: 'White',
+        category: 'BentoCake',
+        price: 60,
+        countInStock: 3,
+        rating: 4.5,
+        numReviews: 4,
+    },
+]
+
+export default products;
